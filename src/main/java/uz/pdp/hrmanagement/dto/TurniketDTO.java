75c221c5a395b8487c13e67f104bfda1594b0e7c
Feruz.Xolov <xolov.2021@mail.ru>
2021-04-23 21:36:29 +0500

    nextCommit

--- src/main/java/uz/pdp/hrmanagement/dto/TurniketDTO.java
+++ src/main/java/uz/pdp/hrmanagement/dto/TurniketDTO.java
@@ -2,9 +2,11 @@ package uz.pdp.hrmanagement.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 public class TurniketDTO {
+    @NotNull
     private UUID userId;
 }
